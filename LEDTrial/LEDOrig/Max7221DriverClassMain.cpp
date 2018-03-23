a73d3d3332d14a976da0a1852d271aa5d95ca23a
TheAliRobertson <2080144r@student.gla.ac.uk>
2018-03-23 17:50:07 +0000

    Updated Driver Class Demo

--- LEDTrial/LEDOrig/Max7221DriverClassMain.cpp
+++ LEDTrial/LEDOrig/Max7221DriverClassMain.cpp
@@ -1,49 +1,50 @@
 #include <iostream>
 #include "Max7221DriverClass.h"
 #include <unistd.h>
+#include <chrono>
+#include <thread>
 
 int main() {
-	
+	printf("Hello, World!\n");
+        Max7219driver driver;
 	while (true)
 	{
-		printf("Hello, World!\n");
-		Max7219driver driver;
-		driver.setColour(Max7219driver::colour::red);
-   		for (int i=0; i<16; i++)
+   		driver.setColour(Max7219driver::colour::red);
+		for (int i=0; i<16; i++)
 		{
 			driver.setIntensity(i);
-			sleep(1);
+			std::this_thread::sleep_for(std::chrono::milliseconds(100));
 		}
 		driver.setColour(Max7219driver::colour::blue);
                 for (int i=0; i<16; i++)
-                {             
+                {
                         driver.setIntensity(i);
-                        sleep(1);
-                }
+                        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		}
 		driver.setColour(Max7219driver::colour::green);
                 for (int i=0; i<16; i++)
-                {             
+                {
                         driver.setIntensity(i);
-                        sleep(1);
+                        std::this_thread::sleep_for(std::chrono::milliseconds(100));
                 }
 		driver.setColour(Max7219driver::colour::yellow);
                 for (int i=0; i<16; i++)
-                {             
+                {
                         driver.setIntensity(i);
-                        sleep(1);
+                        std::this_thread::sleep_for(std::chrono::milliseconds(100));
                 }
 		driver.setColour(Max7219driver::colour::purple);
                 for (int i=0; i<16; i++)
-                {             
+                {
                         driver.setIntensity(i);
-                        sleep(1);
+                        std::this_thread::sleep_for(std::chrono::milliseconds(100));
                 }
 		driver.setColour(Max7219driver::colour::turqoise);
                 for (int i=0; i<16; i++)
-                {             
+                {
                         driver.setIntensity(i);
-                        sleep(1);
+                        std::this_thread::sleep_for(std::chrono::milliseconds(100));
                 }
-    		return 0;
+//    		return 0;
 	}
 }
