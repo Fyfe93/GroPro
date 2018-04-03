30b028db6aad019b9df2fdb9189c24d8790391f8
TheAliRobertson <2080144r@student.gla.ac.uk>
2018-04-03 12:35:31 +0000

    interfacing LED driver classes and OPT classes with PID control loop

---
+++ pidControl/pidExample/OPT.h
@@ -0,0 +1,40 @@
+#include <wiringPi.h>
+#include <wiringPiI2C.h>
+#include <cmath>
+
+//Defining Configuration and Results Register
+
+#define CONF_REG 0x01
+#define RES_REG 0x00
+
+class Opt3001
+{
+public:
+	Opt3001();
+	~Opt3001();
+	void Write(int addr);
+	float Read();
+
+private:
+
+//Defining global variables
+	int m_config;
+	int m_addr;
+	int m_fd;
+
+//Lux Conversion
+	float readLux(int readres)
+	{
+		int left = readres&0xFF; 
+		left = left << 8;
+		int right = readres&0xFF00;
+		right = right >> 8;
+		readres = (left|right);
+
+		int exponential = readres&0b1111000000000000;
+		exponential = exponential >> 12;
+		float LSB_Size=pow(2, exponential)*0.01;
+		int R = readres&0b0000111111111111;
+		return R*LSB_Size;
+	}
+};
--- pidControl/pidExample/main.cpp
+++ pidControl/pidExample/main.cpp
@@ -7,6 +7,7 @@
 //
 
 #include "pid.h"
+#include "OPT.h"
 #include <stdio.h>
 #include <iostream>
 #include <chrono>
@@ -14,19 +15,22 @@
 
 int main(int argc, const char * argv[])
 {
-    
-    PID pid = PID(0.1, 100, -100, 0.1, 0.01, 0.5);
-    
+
+    PID pid = PID(0.1, 1000, -1000, 0.1, 0.01, 0.4);
+    Opt3001 sensor;
+    sensor.Write(0x45);
+
     double val = 20;
-    for (int i = 0; i < 100; i++) {
-        double inc = pid.calculate(0, val);
-        printf("val:% 7.3f inc:% 7.3f\n", val, inc);
-        val += inc;
+    while (true) {
+	float lux = sensor.Read();
+        float inc = pid.calculate(5000, lux);
+        printf("val:% 7.3f inc:% 7.3f\n", lux, inc);
+//        val += inc;
+	inc = (inc / 62.5);
+	printf("increment post scale %f\n\n", inc);
 	std::this_thread::sleep_for(std::chrono::milliseconds(250));
     }
-    
+
     return 0;
 }
-
-    
 
